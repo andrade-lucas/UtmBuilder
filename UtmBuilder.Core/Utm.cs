5e23dc6f0a714299f0a6d233d526b3be3cf753cc
Lucas Andrade <lucasilva108@gmail.com>
2023-03-21 21:33:35 -0300

    chore(utm): create utm base class

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,14 @@
+using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core;
+
+public class UtmBuilder
+{
+    public Url Url { get; set; }
+    public string Source { get; set; }
+    public string Medium { get; set; }
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Term { get; set; }
+    public string Content { get; set; }
+}
