a468deb6a9c8ab1ad9f5dab5edc0824968a93d0f
Lucas Andrade <lucasilva108@gmail.com>
2023-03-22 21:30:47 -0300

    feat(utm): override toString function

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,22 +1,38 @@
+using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
 
 namespace UtmBuilder.Core;
 
 public class Utm
 {
-    /// <summaty>
+    /// <summary>
     /// URL (Website link)
-    /// </summaty>
+    /// </summary>
     public Url Url { get; }
 
-    /// <summaty>
+    /// <summary>
     /// Campaign details
-    /// </summaty>
+    /// </summary>
     public Campaign Campaign { get; }
 
     public Utm(Url url, Campaign campaign)
     {
         Url = url;
         Campaign = campaign;
+    }
+
+    public static implicit operator string(Utm utm) => utm.ToString();
+
+    public override string ToString()
+    {
+        var segments = new List<string>();
+        segments.AddIfNotNull("utm_source", Campaign.Source);
+        segments.AddIfNotNull("utm_medium", Campaign.Medium);
+        segments.AddIfNotNull("utm_campaign", Campaign.Name);
+        segments.AddIfNotNull("utm_id", Campaign.Id);
+        segments.AddIfNotNull("utm_term", Campaign.Term);
+        segments.AddIfNotNull("utm_content", Campaign.Content);
+
+        return $"{Url.Address}?{string.Join("&", segments)}";
     }
 }
