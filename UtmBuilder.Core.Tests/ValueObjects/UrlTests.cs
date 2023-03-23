8f6e94615976fb6d3fb2ef7a202db9108ee03220
Lucas Andrade <lucasilva108@gmail.com>
2023-03-22 23:21:57 -0300

    test(url): pass test cases

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -1,17 +1,26 @@
+using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
 namespace UtmBuilder.Core.Tests.ValueObjects;
 
 [TestClass]
 public class UrlTests
 {
+    const string INVALID_URL = "banana";
+    const string VALID_URL = "https://balta.io";
+    
     [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
     public void should_return_exception_when_url_is_invalid()
     {
-        Assert.Fail();
+        new Url(INVALID_URL);
     }
 
     [TestMethod]
     public void should_not_return_exception_when_url_is_invalid()
     {
-        Assert.Fail();
+        var url = new Url(VALID_URL);
+
+        Assert.AreEqual(VALID_URL, url.Address);
     }
 }
