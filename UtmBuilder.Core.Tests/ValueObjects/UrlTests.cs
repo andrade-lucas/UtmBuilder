423642b4d43824ee4409c1816723ae0a80cec379
Lucas Andrade <lucasilva108@gmail.com>
2023-03-22 22:54:19 -0300

    test(url): start tests

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,17 @@
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    [TestMethod]
+    public void should_return_exception_when_url_is_invalid()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void should_not_return_exception_when_url_is_invalid()
+    {
+        Assert.Fail();
+    }
+}
